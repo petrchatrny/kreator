565e98d16616005a74442753a383911149667a59
Petr Chatrný <posta@petrchatrny.cz>
2025-11-23 01:10:08 +0100

    Add @DtoAttribute implementation

--- kreator-compiler/src/main/kotlin/cz/petrchatrny/kreator/compiler/processor/KreatorProcessor.kt
+++ kreator-compiler/src/main/kotlin/cz/petrchatrny/kreator/compiler/processor/KreatorProcessor.kt
@@ -6,18 +6,21 @@ import com.google.devtools.ksp.processing.*
 import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSClassDeclaration
 import com.google.devtools.ksp.symbol.KSPropertyDeclaration
+import com.google.devtools.ksp.symbol.KSType
 import com.google.devtools.ksp.symbol.Nullability
 import com.squareup.kotlinpoet.*
 import com.squareup.kotlinpoet.ksp.toTypeName
 import com.squareup.kotlinpoet.ksp.writeTo
+import cz.petrchatrny.kreator.annotations.DtoAttribute
 import cz.petrchatrny.kreator.annotations.Kreator
+import java.util.ArrayList
 
+@OptIn(KspExperimental::class)
 class KreatorProcessor(
     private val codeGenerator: CodeGenerator,
     private val logger: KSPLogger
 ) : SymbolProcessor {
 
-    @OptIn(KspExperimental::class)
     override fun process(resolver: Resolver): List<KSAnnotated> {
         // najít všechny třídy anotované anotací @Kreator
         val symbols = resolver.getSymbolsWithAnnotation(Kreator::class.qualifiedName!!)
@@ -77,7 +80,7 @@ class KreatorProcessor(
     private fun generateDtoClass(
         originClass: KSClassDeclaration,
         className: String,
-        properties: List<KSPropertyDeclaration>
+        properties: List<KSPropertyDeclaration>,
     ) {
         // package
         val packageName = originClass.packageName.asString()
@@ -92,8 +95,22 @@ class KreatorProcessor(
         // konstruktor
         val constructorBuilder = FunSpec.constructorBuilder()
         for (prop in properties) {
-            val name = prop.simpleName.asString()
-            val type = prop.type.resolve()
+            // získání použitých anotací jako KSAnnotation
+            val attributeAnnotation = prop.annotations.toList()
+                .filter { ann -> ann.annotationType.resolve().declaration.qualifiedName?.asString() == DtoAttribute::class.qualifiedName }
+                .filter { ann -> (ann.arguments[0].value as ArrayList<String>).contains(className) }
+                .firstOrNull()
+
+            var name: String
+            var type: KSType
+            if (attributeAnnotation != null) {
+                name = attributeAnnotation.arguments[1].value as String
+                type = attributeAnnotation.arguments[2].value as KSType
+            } else {
+                name = prop.simpleName.asString()
+                type = prop.type.resolve()
+            }
+
             val typeName = type.toTypeName().copy(nullable = type.nullability == Nullability.NULLABLE)
 
             val propSpec = PropertySpec.builder(name, typeName)
@@ -112,7 +129,5 @@ class KreatorProcessor(
 
         // zápis souboru na disk
         fileSpecBuilder.build().writeTo(codeGenerator, Dependencies(false, originClass.containingFile!!))
-
-        logger.info("DTO class with name $className was generated")
     }
 }
--- test-jvm/src/main/java/cz/petrchatrny/kreator/test_jvm/mtg/MtgCard.kt
+++ test-jvm/src/main/java/cz/petrchatrny/kreator/test_jvm/mtg/MtgCard.kt
@@ -4,9 +4,9 @@ import cz.petrchatrny.kreator.annotations.Dto
 import cz.petrchatrny.kreator.annotations.DtoAttribute
 import cz.petrchatrny.kreator.annotations.DtoFields
 import cz.petrchatrny.kreator.annotations.Kreator
-import cz.petrchatrny.kreator.test_jvm.mtg.MtgCardFields.id
-import cz.petrchatrny.kreator.test_jvm.mtg.MtgCardFields.name
-import cz.petrchatrny.kreator.test_jvm.mtg.MtgCardFields.type
+//import cz.petrchatrny.kreator.test_jvm.mtg.MtgCardFields.id
+//import cz.petrchatrny.kreator.test_jvm.mtg.MtgCardFields.name
+//import cz.petrchatrny.kreator.test_jvm.mtg.MtgCardFields.type
 import java.math.BigDecimal
 import java.util.UUID
 
@@ -16,7 +16,7 @@ import java.util.UUID
 //)
 @Kreator(
     Dto(name = "MtgCardCreateDto", omit = ["id"]),
-    Dto(name = "MtgCardListDto", pick = ["id", "name", "type"])
+    Dto(name = "MtgCardListDto", pick = ["id", "name", "type", "manaCost"])
 )
 @DtoFields
 class MtgCard(
@@ -26,7 +26,7 @@ class MtgCard(
     val rarity: Int?,
 
     @DtoAttribute("MtgCardCreateDto", name = "requiredMana", type = BigDecimal::class)
-    @DtoAttribute("MtgCardListDto", name = "requiredMana", type = String::class)
+    @DtoAttribute("MtgCardListDto", name = "manaVoe", type = String::class)
     val manaCost: String,
     val rulesText: String?,
     val set: String // použití rezervovaného slova
